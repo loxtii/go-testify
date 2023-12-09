25f9717d766c413bb417c9dfe83411a51319482d
an <loxtii@yandex.ru>
2023-12-09 05:24:20 +0300

    add: tests

--- service_test.go
+++ service_test.go
@@ -3,7 +3,11 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func getResponse(url string) *httptest.ResponseRecorder {
@@ -20,7 +24,13 @@ func TestMainHandlerWhenOk(t *testing.T) {
 	responseRecorder := getResponse("/cafe?count=2&city=moscow")
 
 	// здесь нужно добавить необходимые проверки
+	expectedCode := http.StatusOK
+	expectedCount := 2
+	body := strings.Split(responseRecorder.Body.String(), ",")
 
+	require.NotEmpty(t, responseRecorder.Body)
+	assert.Equal(t, expectedCode, responseRecorder.Code, "expected OK")
+	assert.Len(t, body, expectedCount)
 }
 
 func TestMainHandlerWhenWrongCity(t *testing.T) {
@@ -28,7 +38,12 @@ func TestMainHandlerWhenWrongCity(t *testing.T) {
 	responseRecorder := getResponse("/cafe?count=2&city=omsk")
 
 	// здесь нужно добавить необходимые проверки
+	expectedCode := http.StatusBadRequest
+	body := responseRecorder.Body.String()
 
+	require.NotEmpty(t, responseRecorder.Body)
+	assert.Equal(t, expectedCode, responseRecorder.Code, "expected BadRequest")
+	assert.Equal(t, expectedBody, body)
 }
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
@@ -36,5 +51,30 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	responseRecorder := getResponse("/cafe?count=10&city=moscow")
 
 	// здесь нужно добавить необходимые проверки
+	expectedCode := http.StatusOK
+	body := strings.Split(responseRecorder.Body.String(), ",")
 
+	require.NotEmpty(t, responseRecorder.Body)
+	assert.Equal(t, expectedCode, responseRecorder.Code, "expected OK")
+	assert.Len(t, body, totalCount)
+}
+
+func TestMainHandlerWhenBadCount(t *testing.T) {
+	responseRecorder := getResponse("/cafe?count=number&city=moscow")
+	expectedCode := http.StatusBadRequest
+	body := responseRecorder.Body.String()
+	expectedBody := `wrong count value`
+
+	assert.Equal(t, expectedCode, responseRecorder.Code, "expected BadRequest")
+	assert.Equal(t, expectedBody, body)
+}
+
+func TestMainHandlerWhenEmptyCount(t *testing.T) {
+	responseRecorder := getResponse("/cafe?count=&city=moscow")
+	expectedCode := http.StatusBadRequest
+	body := responseRecorder.Body.String()
+	expectedBody := `count missing`
+
+	assert.Equal(t, expectedCode, responseRecorder.Code, "expected BadRequest")
+	assert.Equal(t, expectedBody, body)
 }
